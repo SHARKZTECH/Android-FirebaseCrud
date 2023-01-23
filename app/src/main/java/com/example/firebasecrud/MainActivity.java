1333d3f8eb5ea2d836f736d8fa54825261d1e64a
sharkz <mosenjemudogoz@gmail.com>
2023-01-23 12:33:10 +0300

    toolbar,menuitem,logout

--- app/src/main/java/com/example/firebasecrud/MainActivity.java
+++ app/src/main/java/com/example/firebasecrud/MainActivity.java
@@ -9,12 +9,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -37,17 +41,24 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton floatingActionButton;
     ProgressBar progressBar;
+    MaterialToolbar toolbar;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar=findViewById(R.id.toolbar);
         progressBar=findViewById(R.id.progressBar);
         floatingActionButton=findViewById(R.id.addCourse);
         recyclerView=findViewById(R.id.recycler);
         fDb=FirebaseDatabase.getInstance();
         dbRef=fDb.getReference("Courses");
         coursesList=new ArrayList<>();
+        mAuth=FirebaseAuth.getInstance();
+
+        setSupportActionBar(toolbar);
+
 
         floatingActionButton.setOnClickListener(view -> {
             startActivity(new Intent(this,AddActivity.class));
@@ -61,6 +72,25 @@ public class MainActivity extends AppCompatActivity {
         progressBar.setVisibility(View.VISIBLE);
         getCourses();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                mAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
